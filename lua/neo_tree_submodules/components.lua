49b60dc8fefac441d70b36f301190a12ab2be7dc
apple1417 <git@apple1417.dev>
2025-07-19 14:41:13 +1200

    get submodules listing

--- lua/neo_tree_submodules/components.lua
+++ lua/neo_tree_submodules/components.lua
@@ -3,21 +3,31 @@ local common = require("neo-tree.sources.common.components")
 
 local M = {}
 
+M.icon = function(config, node, state)
+    if vim.startswith(node.id, "neo_tree_submodules:clean") then
+        return {
+            text = "",
+            highlight = highlights.GIT_IGNORED,
+        }
+    end
+
+    return common.icon(config, node, state)
+end
+
 M.name = function(config, node, state)
     local highlight = config.highlight or highlights.FILE_NAME_OPENED
     local name = node.name
-    if node.type == "directory" then
+
+    if vim.startswith(node.id, "neo_tree_submodules:clean") then
+        highlight = highlights.GIT_IGNORED
+    elseif node.type == "directory" then
         if node:get_depth() == 1 then
             highlight = highlights.ROOT_NAME
-            if node:has_children() then
-                name = "GIT STATUS for " .. name
-            else
-                name = "GIT STATUS (working tree clean) for " .. name
-            end
         else
             highlight = highlights.DIRECTORY_NAME
         end
     elseif config.use_git_status_colors then
+        -- todo
         local git_status = state.components.git_status({}, node, state)
         if git_status and git_status.highlight then
             highlight = git_status.highlight
--- lua/neo_tree_submodules/git.lua
+++ lua/neo_tree_submodules/git.lua
@@ -1,53 +1,137 @@
 local renderer = require("neo-tree.ui.renderer")
 local file_items = require("neo-tree.sources.common.file-items")
-local log = require("neo-tree.log")
-local git = require("neo-tree.git")
-
-local utils = require("neo-tree.utils")
-
 
 local M = {}
 
-local function list_submodules()
-    local ok, git_output = utils.execute_command({ "git", "submodule", "status", "--recursive" })
-    if not ok then
-        log.trace("GIT SUBMODULE STATUS ERROR ", git_output)
+
+local function find_git_root()
+    local ret = vim.system(
+        { "git", "rev-parse", "--show-toplevel" },
+        { text=true }
+    ):wait()
+    if ret.code ~= 0 or ret.stdout == nil or #ret.stdout == 0 then
+        -- error
+        return nil
+    end
+    return vim.fs.abspath(vim.trim(ret.stdout))
+end
+
+local function list_submodules(git_root)
+    git_root = git_root or "."
+
+    local ret = vim.system(
+        { "git", "submodule", "status", "--recursive" },
+        { cwd=git_root, text=true }
+    ):wait()
+
+    if ret.code ~= 0 then
+        -- error
         return {}
     end
-    if not git_output then
+    if ret.stdout == nil or #ret.stdout == 0 then
+        -- no submodules
         return {}
     end
+
     local submodules_paths = {}
-    for _, line in pairs(git_output) do
-        local parts = vim.split(line, " ")
-        table.insert(submodules_paths, parts[3])
+    for _, line in pairs(vim.split(ret.stdout, "\n")) do
+        local match = line:match("^.[0-9a-fA-F]+ (.+) %(..-%)$")
+        if match ~= nil then
+            local path = vim.fs.abspath(vim.fs.joinpath(git_root, match))
+            table.insert(submodules_paths, path)
+        end
     end
     return submodules_paths
 end
 
----Get a table of all open buffers, along with all parent paths of those buffers.
----The paths are the keys of the table, and all the values are 'true'.
+local function get_unique_path_names(paths)
+    local is_windows = vim.fn.has("win32")
+    local path_separator = is_windows and "\\" or "/"
+
+    local path_data = vim.iter(paths):map(function(path)
+        -- on windows do both path seperators
+        if is_windows then path = path:gsub("/", "\\") end
+
+        local parts = vim.iter(vim.split(path, path_separator)):rev():totable()
+        return { n = 1, parts=parts }
+    end):totable()
+
+    for i = 1, #path_data do
+        local path_a = path_data[i]
+        for j = i + 1, #path_data do
+            local path_b = path_data[j]
+
+            local n = 1
+            while
+                n <= #path_a.parts and n <= #path_b.parts
+                and path_a.parts[n] == path_b.parts[n]
+            do
+                n = n + 1
+                path_a.n = math.max(path_a.n, n)
+                path_b.n = math.max(path_b.n, n)
+            end
+        end
+    end
+
+    return vim.iter(path_data):map(function(data)
+        local parts = vim.iter(data.parts):slice(1, data.n):rev():totable()
+        return vim.fs.joinpath(unpack(parts))
+    end):totable()
+end
+
 M.get_git_status = function(state)
     if state.loading then
         return
     end
     state.loading = true
-    local status_lookup, project_root = git.status(state.git_base, true, state.path)
+    state.default_expanded_nodes = {}
 
-    local submodules = list_submodules()
+    local git_root = find_git_root()
+
+    local repos = list_submodules(git_root)
+    table.insert(repos, 1, git_root)
+
+    local unique_repo_names = get_unique_path_names(repos)
+
+    local repo_data = vim.iter(repos):enumerate():map(function(i, path)
+        local context = file_items.create_context()
+        context.state = state
+
+        return {
+            path = path,
+            name = unique_repo_names[i],
+            context = context,
+            root = nil,
+            status = {},
+        }
+    end):totable()
+
+    for i, data in pairs(repo_data) do
+        data.root = file_items.create_item(data.context, data.path, "directory")
+        data.root.name = data.name
+        data.root.loaded = true
+        data.root.search_pattern = state.search_pattern
+        data.context.folders[data.root.path] = data.root
 
 
-    state.path = project_root or state.path or vim.fn.getcwd()
-    local context = file_items.create_context()
-    context.state = state
-    -- Create root folder
-    local root = file_items.create_item(context, state.path, "directory")
-    root.name = vim.fn.fnamemodify(root.path, ":~")
-    root.loaded = true
-    root.search_pattern = state.search_pattern
-    context.folders[root.path] = root
+        -- get status
 
-    for path, status in pairs(status_lookup) do
+        if #data.status == 0 then
+            table.insert(data.root.children, {
+                id = "neo_tree_submodules:clean:" .. i,
+                name = "clean",
+                type = "directory",
+                children = {},
+            })
+        else
+            -- not clean so auto expand
+            table.insert(state.default_expanded_nodes, data.root.id)
+        end
+
+        file_items.advanced_sort(data.root.children, state)
+    end
+
+    --[[for path, status in pairs(status_lookup) do
         local success, item = pcall(file_items.create_item, context, path, "file")
         item.status = status
         if success then
@@ -64,15 +148,18 @@ M.get_git_status = function(state)
         if not success then
             log.error("Error creating item for " .. path .. ": " .. item)
         end
-    end
+    end]]
 
-    state.git_status_lookup = status_lookup
-    state.default_expanded_nodes = {}
-    for id, _ in pairs(context.folders) do
-        table.insert(state.default_expanded_nodes, id)
-    end
-    file_items.advanced_sort(root.children, state)
-    renderer.show_nodes({ root }, state)
+    state.path = git_root or state.path or vim.fn.getcwd()
+    state.repo_data = repo_data
+
+    renderer.show_nodes(
+        vim.iter(repo_data):map(function(data)
+            return data.root
+        end):totable(),
+        state
+    )
+
     state.loading = false
 end
 
